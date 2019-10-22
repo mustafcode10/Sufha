4d49bb0df6571d5305ec04512b1393bde8913b6b
mustafcode10 <mustafdiiriye@hotmail.com>
2019-10-22 20:08:33 +0300

    validation of registration and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,12 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 const User = require('../../model/User');
 
+
+// load input validation
+const validateRegisterInput = require('../../validator/register');
+const validateLoginInput = require('../../validator/login');
+
+
 // Route   GET api/users/test
 // @desc   tests users route
 // @access Public
@@ -16,11 +22,21 @@ router.get('/test', (req, res)=>res.json({msg: 'user works'}));
 // @decs   registar user
 // @access Public
 router.post('/register', (req, res)=> {
+    // validation input
+    const {errors, isValid} = validateRegisterInput(req.body);
+    // Check validation is error
+    if(!isValid){
+        return res.status(400).json(errors)
+    }
+
     User.findOne({
         email: req.body.email })
        .then(user => {
            if(user){
-            return res.status(400).json({email: 'Email  already exists'})
+               errors.email = 'Email already exists'
+            // return res.status(400).json({email: 'Email  already exists'})
+            return res.status(400).json(errors)
+
            } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200', // size
@@ -63,6 +79,12 @@ router.post('/register', (req, res)=> {
 
 
 router.post('/login', (req, res)=> {
+      // validation input
+        const {errors, isValid} = validateLoginInput(req.body);
+        // Check validation is error
+        if(!isValid){
+            return res.status(400).json(errors)
+        }
     const email = req.body.email;
     const password = req.body.password;
 
@@ -71,7 +93,9 @@ router.post('/login', (req, res)=> {
 
     // Check by user
         if(!user){
-        return res.status(404).json({email: 'User not found'})
+            errors.email = 'User not found'
+        // return res.status(404).json({email: 'User not found'})
+        return res.status(404).json(errors)
      }
 
     // check password
@@ -97,7 +121,9 @@ router.post('/login', (req, res)=> {
             })
             
         }else {
-            return res.status(400).json({password: 'password incorrect'})
+            errors.password = 'Password incorrect'
+            // return res.status(400).json({password: 'password incorrect'})
+            return res.status(400).json(errors)
         }
     })
     })
